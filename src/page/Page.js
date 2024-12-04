a27c625888d89626da129b85656f700db81c850b
joaoalbertocsjunior <joaoalbertocsjunior@gmail.com>
2024-12-04 05:31:00 -0300

    Update Page.js

--- src/page/Page.js
+++ src/page/Page.js
@@ -1,8 +1,8 @@
-import { Element } from "@joaoalbertocsjunior/react-s";
+import { JSONDOM } from "./react-s";
 
-const e = new Element({ element: 'div' });
+const e = new JSONDOM({ element: 'div' });
 e.id = 'e';
 e.onClick = () => { alert(true) };
 e.children = 'Hello World';
 
-export default [e];
+export default e;
